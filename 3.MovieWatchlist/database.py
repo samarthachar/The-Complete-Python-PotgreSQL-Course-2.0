7261b12e98da9062244bdde89c1552c37833a9c3
Samarth <achar.samarth@gmail.com>
2025-12-08 20:39:00 +0000

    Finished Migration to PostgreSQL

--- 3.MovieWatchlist/database.py
+++ 3.MovieWatchlist/database.py
@@ -1,51 +1,60 @@
-import datetime, sqlite3
+import datetime
 from sql import *
-# title, release_Date, watched
+import os
+import psycopg2
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 CREATE_RELEASE_INDEX = "CREATE INDEX IF NOT EXISTS idx_movies_release ON movies(release_timestamp)"
 
 
-connection = sqlite3.connect("data.db")
+connection = psycopg2.connect(os.environ["DATABASE_URL"])
 
 def create_tables():
     with connection:
-        connection.execute(CREATE_MOVIES_TABLE)
-        connection.execute(CREATE_USERS_TABLE)
-        connection.execute(CREATE_WATCHED_TABLE)
-        connection.execute(CREATE_RELEASE_INDEX)
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_MOVIES_TABLE)
+            cursor.execute(CREATE_USERS_TABLE)
+            cursor.execute(CREATE_WATCHED_TABLE)
+            cursor.execute(CREATE_RELEASE_INDEX)
 
 def add_user(username):
     with connection:
-        connection.execute(INSERT_USER, (username,))
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_USER, (username,))
 
 
 def add_movie(title, release_timestamp):
     with connection:
-        connection.execute(INSERT_MOVIES, (title, release_timestamp))
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_MOVIES, (title, release_timestamp))
 
 def get_movies(upcoming=False):
     with connection:
-        cursor = connection.cursor()
-        if upcoming:
-            today_timestamp = datetime.datetime.today().timestamp()
-            cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
-        else:
-            cursor.execute(SELECT_ALL_MOVIES)
-        return cursor.fetchall()
+        with connection.cursor() as cursor:
+            if upcoming:
+                today_timestamp = datetime.datetime.today().timestamp()
+                cursor.execute(SELECT_UPCOMING_MOVIES, (today_timestamp,))
+            else:
+                cursor.execute(SELECT_ALL_MOVIES)
+            return cursor.fetchall()
 
 def search_movies(search_term):
     with connection:
-        cursor = connection.execute(SEARCH_MOVIES, (f"%{search_term}%",))
-        return cursor.fetchall()
+        with connection.cursor() as cursor:
+            cursor.execute(SEARCH_MOVIES, (f"%{search_term}%",))
+            return cursor.fetchall()
 
 
 def watch_movie(username, movie_id):
     with connection:
-        connection.execute(INSERT_WATCHED_MOVIE, (username, movie_id))
+        with connection.cursor() as cursor:
+            cursor.execute(INSERT_WATCHED_MOVIE, (username, movie_id))
 
 def get_watched_movies(username):
     with connection:
-        cursor = connection.cursor()
-        cursor.execute(SELECT_WATCHED_MOVIES, (username,))
-        return cursor.fetchall()
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_WATCHED_MOVIES, (username,))
+            return cursor.fetchall()
--- 3.MovieWatchlist/sql.py
+++ 3.MovieWatchlist/sql.py
@@ -1,5 +1,5 @@
 CREATE_MOVIES_TABLE = """CREATE TABLE IF NOT EXISTS movies (
-    id INTEGER PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     title TEXT,
     release_timestamp REAL
 );"""
@@ -11,20 +11,20 @@ CREATE_USERS_TABLE = """CREATE TABLE IF NOT EXISTS users (
 CREATE_WATCHED_TABLE = """ CREATE TABLE IF NOT EXISTS watched (
     user_username TEXT,
     movie_id INTEGER,
-    FOREIGN KEY(user_username) REFERENCES users(id),
+    FOREIGN KEY(user_username) REFERENCES users(username),
     FOREIGN KEY(movie_id) REFERENCES movies(id)
 );"""
 
-INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp) VALUES (?, ?);"
-INSERT_USER = "INSERT INTO users (username) VALUES (?)"
-DELETE_MOVIE = "DELETE FROM movies WHERE title = ?;"
+INSERT_MOVIES = "INSERT INTO movies (title, release_timestamp) VALUES (%s, %s);"
+INSERT_USER = "INSERT INTO users (username) VALUES (%s)"
+DELETE_MOVIE = "DELETE FROM movies WHERE title = %s;"
 SELECT_ALL_MOVIES = "SELECT * FROM movies"
-SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
+SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > %s;"
 SELECT_WATCHED_MOVIES = """SELECT id, title, release_timestamp 
 FROM movies
 JOIN watched ON movies.id = watched.movie_id
 JOIN users on users.username = watched.user_username
-WHERE username = ?;"""
-INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id) VALUES (? , ?);"
-SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 where title = ?;"
-SEARCH_MOVIES = "SELECT * FROM movies WHERE title LIKE ?;"
+WHERE username = %s;"""
+INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id) VALUES (%s , %s);"
+SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 where title = %s;"
+SEARCH_MOVIES = "SELECT * FROM movies WHERE title LIKE %s;"
